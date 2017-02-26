bbe5e54c664d805c6989e1a76e4d4fb3d21e6957
stella-maris <mango.gal@gmail.com>
2017-02-25 22:59:58 -0800

    Adds select, with default filter value, and orderBy filter

---
+++ second_exercise/js/controllers.js
@@ -0,0 +1,20 @@
+// Register our controller with the angular module.
+var cardApp = angular.module('cardApp', []);
+
+// The controller is a constructor function that takes a $scope parameter.
+// The controller lets us establish data binding between the controller and the view.
+// The model is initialized with the $scope parameter.
+cardApp.controller('MainController', ['$scope', function ($scope) {
+    // Scope ensures that any changes to the 
+    // model are reflected in the controller.
+    // Here we create an initialize a 'title' model.
+    $scope.greeting = "AngularJS Hello World!";
+    $scope.orderProp = "numOrd";
+    // Define cards model which stores an array of objects.
+    $scope.cards = [
+        { "number": "2", "suit": "Hearts", "numOrd": 2 },
+        { "number": "10", "suit": "Spades", "numOrd": 10 },
+        { "number": "5", "suit": "Spades", "numOrd": 5 },
+        { "number": "Q", "suit": "Hearts", "numOrd": 12 }
+    ];
+}]);
