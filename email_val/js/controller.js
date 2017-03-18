77ea78a4739db3e58e8b0129464499b3d62d511c
stella-maris <mango.gal@gmail.com>
2017-03-17 23:11:34 -0700

    adds validation on submit, returns success message

--- email_val/js/controller.js
+++ email_val/js/controller.js
@@ -6,15 +6,22 @@ var myControllers = (function () {
     ctrls.controller('AppCtrl', ['$scope', function($scope) {
         $scope.title ="Assignment 2";
     }]);
-    
+    //child controller
     ctrls.controller('EmailCtrl', ['$scope', function($scope) {
+        $scope.isClicked = false;
+        $scope.emailVerified = false;
         $scope.update = function (email, subject, message) {
+                $scope.isClicked = true;
                 console.log("email is: ", email);
                 console.log("subject is: ", subject);
                 console.log("message is: ", message);
+            if (email && subject && message) {
+                $scope.emailVerified = true;
+                $scope.sentEmail = email;
+            }
         };
     }]);
-    
+    //child controller
     ctrls.controller('AboutCtrl', ['$scope', function ($scope) {
         $scope.name = "Mary Lee"
     }]);
