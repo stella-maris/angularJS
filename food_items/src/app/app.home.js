660c25341ab9170e16d9bf30c21f007a1777a114
stella-maris <mango.gal@gmail.com>
2017-03-20 16:14:04 -0700

    modifies home component

--- food_items/src/app/app.home.js
+++ food_items/src/app/app.home.js
@@ -9,16 +9,35 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var FoodItem = (function () {
+    function FoodItem() {
+    }
+    return FoodItem;
+}());
+exports.FoodItem = FoodItem;
 var HomeComponent = (function () {
     function HomeComponent() {
+        this.items = FOODITEMS;
     }
+    HomeComponent.prototype.onSelect = function (item) {
+        //console.log(this);
+        this.selectedCard = item;
+        alert(item.name);
+    };
     HomeComponent = __decorate([
         core_1.Component({
-            template: "This is HOME."
+            templateUrl: './app/home.html'
         }), 
         __metadata('design:paramtypes', [])
     ], HomeComponent);
     return HomeComponent;
 }());
 exports.HomeComponent = HomeComponent;
+// Define card data.
+var FOODITEMS = [
+    { id: 1, name: "Cucumbers", mfg: "Hot House", pkg: "10 kg", qty: 6 },
+    { id: 2, name: "Carrots", mfg: "Hot House", pkg: "8 kg", qty: 14 },
+    { id: 3, name: "Cheese", mfg: "Hot House", pkg: "5 kg", qty: 20 },
+    { id: 4, name: "French Fries", mfg: "McCain", pkg: "15 kg", qty: 8 }
+];
 //# sourceMappingURL=app.home.js.map
--- food_items/src/app/app.home.ts
+++ food_items/src/app/app.home.ts
@@ -1,5 +1,29 @@
 import { Component } from '@angular/core';
+
+export class FoodItem {
+    id: number;
+    name: string;
+}
+
 @Component({
-    template: `This is HOME.`
+    templateUrl: './app/home.html'
 })
-export class HomeComponent { }
+export class HomeComponent {
+        public items = FOODITEMS; 
+    selectedCard: FoodItem;
+	
+    onSelect(item: FoodItem) { 
+        //console.log(this);
+        this.selectedCard = item;
+        alert(item.name);
+    }
+ }
+// Define card data.
+var FOODITEMS: FoodItem[] = [
+    { id: 1, name: "Cucumbers", mfg: "Hot House", pkg: "10 kg", qty: 6 },
+    { id: 2, name: "Carrots", mfg: "Hot House", pkg: "8 kg", qty: 14 },
+    { id: 3, name: "Cheese", mfg: "Hot House", pkg: "5 kg", qty: 20 },
+    { id: 4, name: "French Fries", mfg: "McCain", pkg: "15 kg", qty: 8 }
+];
+
+
